685ae201bc23ec5cae3942ccf03538a252ebcd1a
Javarris Barnett <javarrisb@gmail.con>
2022-02-08 03:51:11 +0100

    created controllers

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,7 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb: // localhost:27017/pizza-hunt', {
+    usedNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
-
